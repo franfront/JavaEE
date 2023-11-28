0ca2ed2892c78491ef56fbfd082c7ba1c091474a
PC <fernandezfrancoa2001@gmail.com>
2023-11-28 20:50:52 -0300

    Hibernate - JPQL

--- hibernate-jpa-app/src/main/java/org/ffernandez/hibernateapp/HibernateQL.java
+++ hibernate-jpa-app/src/main/java/org/ffernandez/hibernateapp/HibernateQL.java
@@ -27,6 +27,37 @@ public class HibernateQL {
                     .setParameter("idCliente", 6L)
                     .getSingleResult();
 
+            System.out.println(nombre);
+
+            System.out.println("===== consulta por campos personalizados =====");
+
+            Object[] campos = em.createQuery("select c.id, c.nombre, c.apellido from Cliente c where c.id = :idCliente", Object[].class)
+                    .setParameter("idCliente", 6L)
+                    .getSingleResult();
+
+            Long id = (Long) campos[0];
+            String nombre2 = (String) campos[1];
+            String apellido = (String) campos[2];
+
+            System.out.println("id: " + id + ", nombre: " + nombre2 + ", apellido: " + apellido);
+
+            System.out.println("===== consulta por campos personalizados lista =====");
+
+            List<Object[]> registros = em.createQuery("select c.id, c.nombre, c.apellido from Cliente c", Object[].class)
+                    .getResultList();
+
+           // for(Object[] registro : registros) {
+                registros.forEach(reg->{
+                Long id2 = (Long) reg[0];
+                String nombre3 = (String) reg[1];
+                String apellido2 = (String) reg[2];
+                System.out.println("id: " + id2 + ", nombre: " + nombre3 + ", apellido: " + apellido2);
+                });
+           // }
+
+
+            em.close();
+
 
 
         }
