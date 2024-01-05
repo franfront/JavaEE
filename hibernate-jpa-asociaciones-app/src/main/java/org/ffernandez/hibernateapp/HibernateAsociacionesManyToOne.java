734d486d692ac2ddb31417de9e90b8fd1e084d20
PC <fernandezfrancoa2001@gmail.com>
2024-01-04 23:47:46 -0300

    Hibernate - Asociaciones

---
+++ hibernate-jpa-asociaciones-app/src/main/java/org/ffernandez/hibernateapp/HibernateAsociacionesManyToOne.java
@@ -0,0 +1,38 @@
+package org.ffernandez.hibernateapp;
+
+import jakarta.persistence.EntityManager;
+import org.ffernandez.hibernateapp.entity.Cliente;
+import org.ffernandez.hibernateapp.entity.Factura;
+import org.ffernandez.hibernateapp.util.JpaUtil;
+
+public class HibernateAsociacionesManyToOne {
+    public static void main(String[] args) {
+
+        EntityManager em = JpaUtil.getEntityManager();
+
+
+        try{
+            em.getTransaction().begin();
+            Cliente cliente = new Cliente("Eduardo", "Argento");
+            cliente.setFormaPago("debito");
+            em.persist(cliente);
+
+
+
+            Factura factura = new Factura("Compras de oficina", 1000L);
+            factura.setCliente(cliente);
+            em.persist(factura);
+
+
+            em.getTransaction().commit();
+
+        } catch (Exception e){
+            em.getTransaction().rollback();
+
+        }finally {
+            em.close();
+        }
+
+
+    }
+}
