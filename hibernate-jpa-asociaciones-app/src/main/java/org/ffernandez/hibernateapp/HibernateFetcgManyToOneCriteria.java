e3d6d4ad3649c0d84ece8922baaa40d0adbf32f3
PC <fernandezfrancoa2001@gmail.com>
2024-02-29 03:12:30 -0300

    Hibernate - API Criteria

---
+++ hibernate-jpa-asociaciones-app/src/main/java/org/ffernandez/hibernateapp/HibernateFetcgManyToOneCriteria.java
@@ -0,0 +1,32 @@
+package org.ffernandez.hibernateapp;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.ffernandez.hibernateapp.entity.Factura;
+import org.ffernandez.hibernateapp.util.JpaUtil;
+
+import java.util.List;
+
+public class HibernateFetcgManyToOneCriteria {
+    public static void main(String[] args) {
+
+        EntityManager em = JpaUtil.getEntityManager();
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Factura> query = cb.createQuery(Factura.class);
+
+        Root<Factura> factura = query.from(Factura.class); // indica la base de la consulta
+        query.select(factura);
+
+        List<Factura> facturas = em.createQuery(query).getResultList();
+
+
+        em.close();
+
+
+
+
+    }
+}
